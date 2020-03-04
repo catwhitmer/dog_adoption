905aa9e96b5ab254b8b8aa05b47df95713ed83c4
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-04 16:55:10 +0000

    first commit

---
+++ lib/dog_adoption.rb
@@ -0,0 +1,6 @@
+require "dog_adoption/version"
+
+module DogAdoption
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dog_adoption/version.rb
@@ -0,0 +1,3 @@
+module DogAdoption
+  VERSION = "0.1.0"
+end
