8ff861a31b97538596c676a8d3f81e3d2367ea1d
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-04 18:48:17 +0000

    dogs and dog list methods

--- lib/dog_adoption/cli.rb
+++ lib/dog_adoption/cli.rb
@@ -2,30 +2,23 @@ class DogAdoption::CLI
   
   def start 
     puts "Welcome to Cat's Dog Adoption!"
-    break
-    puts "For a list of dogs available, type list."
-    puts "To leave anytime, type exit."
-    
-  end
     dog_list
+    dogs
     
-    
-    puts "To leave anytime, type exit."
-    
-    input = gets.strip.downcase
   end
   
   def dog_list
     #get dog list from scraping
     #return array of dogs
-    puts "These are the dogs available today."
     puts "For a list of dogs available, type list."
+    puts "These are the dogs available today."
   end
   
   def dogs 
     input = nil 
     while input != "exit"
       puts "Please enter the number of the dog you would like more information about:"
+      puts "To leave anytime, type exit."
       input = gets.strip.downcase
       case input
       when "1"
@@ -40,7 +33,7 @@ class DogAdoption::CLI
         puts "What was that?"
       end
     end
-    
+
   end
   
 end
